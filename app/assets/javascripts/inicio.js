48bdf1f2c756c06ca0e4de326edceb81d9b26fb9
colmexdev <github@colmex.mx>
2017-07-04 12:22:53 -0500

    Primer intento de slider eventos

---
+++ app/assets/javascripts/inicio.js
@@ -0,0 +1,14 @@
+function scaleEvents(big,med,small){
+	try{
+		if(small == ""){
+			$("#div_eventos").css("height","0");
+		}
+		else{
+			if(ancho < 785){ document.getElementById("render_eventos").innerHTML = small; }
+			else if(ancho < 1150){ document.getElementById("render_eventos").innerHTML = med; }
+			else{ document.getElementById("render_eventos").innerHTML = big;}
+		}
+	}
+	catch(err){
+	}
+}
