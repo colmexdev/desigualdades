ad717b2e7abb94005091d14a96b97f4b8163486e
colmexdev <github@colmex.mx>
2017-07-10 18:00:39 -0500

    Integrando slider

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -7,7 +7,7 @@ class InicioController < ApplicationController
       cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.220', port: '1433'
       @resultado = cliente.execute("USE Agenda")
       @resultado.do
-      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosDesigualdades  ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
+      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosDesigualdades  ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') DESC, horaInicio DESC")
       @ev_big, @ev_small, @ev_tiny = construye_slider_eventos(@resultado)
     rescue => e
       logger.debug e.message
