3c433a3f9cfe947bc16a26e272541a7a4a483b3d
colmexdev <github@colmex.mx>
2018-06-08 12:17:17 -0500

    Orden de videos

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -107,3 +107,186 @@ function scaleEvents(big,med,small){
 		html += '</div>';
 		parent.html(html);
 	}
+
+$(document).on("ready page:load", function(){
+	if(window.location.pathname == "/informe2018"){
+
+		$(window).on("resize",function(){
+				// init Isotope
+						var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+						var $gellary_img = $('.all-gallery-items').isotope({
+							masonry: {
+								columnWidth: $(".all-gallery-items").width()/(ancho >= 1200 ? 3 : (ancho >= 680 ? 2 : 1))
+							},
+							itemSelector: '.single-item',
+							percentPosition: true,
+							transitionDuration: '0.8s',
+							getSortData: {
+							name: '.name',
+							symbol: '.symbol',
+							number: '.number parseInt',
+							category: '[data-category]',
+							weight: function( itemElem ) {
+								var weight = $( itemElem ).find('.weight').text();
+								return parseFloat( weight.replace( /[\(\)]/g, '') );
+							}
+							}
+						});  
+
+						$(".single-item").width($(".all-gallery-items").width()/(ancho >= 1200 ? 3 : (ancho >= 680 ? 2 : 1))).css({"margin-bottom":"20px"});     
+						
+						// filter functions
+						var filterFns = {
+							// show if number is greater than 50
+							numberGreaterThan50: function() {
+							var number = $(this).find('.number').text();
+							return parseInt( number, 10 ) > 50;
+							},
+							// show if name ends with -ium
+							ium: function() {
+							var name = $(this).find('.name').text();
+							return name.match( /ium$/ );
+							}
+						};
+						
+						// bind filter button click
+						$('.gallery-menu ul').on( 'click', 'li', function() {
+							var filterValue = $( this ).attr('data-filter');
+							// use filterFn if matches value
+							filterValue = filterFns[ filterValue ] || filterValue;
+							$gellary_img.isotope({ filter: filterValue });
+						});            
+
+						// Isotop for four grid     
+						var $gellary_img_4 = $('.gallery-grid-4').isotope({
+							itemSelector: '.single-item-4',
+							transitionDuration: '0.8s',
+							getSortData: {
+							name: '.name',
+							symbol: '.symbol',
+							number: '.number parseInt',
+							category: '[data-category]',
+							weight: function( itemElem ) {
+								var weight = $( itemElem ).find('.weight').text();
+								return parseFloat( weight.replace( /[\(\)]/g, '') );
+							}
+							}
+						}); 
+						// bind filter button click
+						$('.gallery-menu ul').on( 'click', 'li', function() {
+							var filterValue = $( this ).attr('data-filter');
+							// use filterFn if matches value
+							filterValue = filterFns[ filterValue ] || filterValue;
+							$gellary_img_4.isotope({ filter: filterValue });
+						});
+					
+						// change is-checked class on buttons
+						$('.gallery-menu ul').each( function( i, liList ) {
+							var $liList = $( liList );
+							$liList.on( 'click', 'li', function() {
+							$liList.find('.is_selected').removeClass('is_selected');
+							$( this ).addClass('is_selected');
+							});
+						});
+		});
+
+		$.ajax({
+			url: "http://pred1.colmex.mx/catalogo_videos.json?titulo=Informe%20Desigualdades%20en%20M%C3%A9xico%202018.%20Dos%20historias%20de%20vida.",
+			method: "get",
+			dataType: "JSON",
+			success: function(result){
+				var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:grupoUnoVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
+				$("#vid-historias").html(html);
+			}
+		});
+
+		$.ajax({
+			url: "http://pred1.colmex.mx/catalogo_videos.json?titulo=Desigualdades%20en%20M%C3%A9xico%202018.%20Entrecruzamiento%20y%20acumulaci%C3%B3n%20de%20desventajas",
+			method: "get",
+			dataType: "JSON",
+			success: function(result){
+				var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:grupoUnoVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
+				$("#vid-talk").html(html);
+			}
+		});
+
+		$.ajax({
+			url: "http://pred1.colmex.mx/catalogo_videos.json?titulo=Lanzamiento%20del%20informe%20Desigualdades%20en%20M%C3%A9xico%202018",
+			method: "get",
+			dataType: "JSON",
+			success: function(result){
+				var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:grupoUnoVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
+				$("#vid-stream").html(html);
+			}
+		});
+
+	} else if(window.location.pathname == "/videos"{
+
+		$.ajax({
+			method: 'get',
+			dataType: 'JSON',
+			url: 'http://pred1.colmex.mx/catalogo_videos.json?lista=Seminario%20sobre%20Trabajo%20y%20Desigualdades&all=true&crono=desc',
+			success: function(result){
+				var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+				window.trades_vids = result;
+				drawSlider($("#trades-slider"),result["vids"].length,"trades",3)
+				renderSlider("trades",$("#trades-container"),$("#trades-cont"),$("#trades-vids"),result["vids"],"trades-vid-cont","trades-slide-vid",(ancho < 792 ? 2 : 3),(ancho < 792 ? 0.4 : 0.3),false,"trades");
+
+		$.ajax({
+			method: 'get',
+			dataType: 'JSON',
+			url: 'http://pred1.colmex.mx/catalogo_videos.json?lista=Seminario%20Migraci%C3%B3n,%20Desigualdad%20y%20Pol%C3%ADticas%20P%C3%BAblicas&all=true&crono=desc',
+			success: function(result){
+				var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+				window.migdep_vids = result;
+				drawSlider($("#migdep-slider"),result["vids"],"migdep",3)
+				renderSlider("migdep",$("#migdep-container"),$("#migdep-cont"),$("#migdep-vids"),result["vids"],"migdep-vid-cont","migdep-slide-vid",(ancho < 792 ? 2 : 3),(ancho < 792 ? 0.4 : 0.3),false,"migdep");
+
+				$.ajax({
+					method: 'get',
+					dataType: 'JSON',
+					url: 'http://pred1.colmex.mx/catalogo_videos.json?lista=Seminario%20sobre%20Desigualdad%20Socioecon%C3%B3mica%20y%20Educativa&all=true&crono=desc',
+					success: function(result){
+						var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+						window.sedes_vids = result;
+						drawSlider($("#sedes-slider"),result["vids"],"sedes",3)
+						renderSlider("sedes",$("#sedes-container"),$("#sedes-cont"),$("#sedes-vids"),result["vids"],"sedes-vid-cont","sedes-slide-vid",(ancho < 792 ? 2 : 3),(ancho < 792 ? 0.4 : 0.3),false,"sedes");
+
+						$(window).on("resize",function(){
+							var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+							renderSlider("sedes",$("#sedes-container"),$("#sedes-cont"),$("#sedes-vids"),result["vids"],"sedes-vid-cont","sedes-slide-vid",(ancho >= 792 ? 3 : (ancho >= 545 ? 2 : 1)),(ancho >= 792 ? 0.3 : (ancho >= 545 ? 0.4 : 0.47)),true,"sedes");
+						});
+
+					$(document).ready(function($) {
+						$('a[data-rel^=lightcase]').lightcase();
+					});
+
+					}
+				});
+
+				$(window).on("resize",function(){
+							var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+							renderSlider("migdep",$("#migdep-container"),$("#migdep-cont"),$("#migdep-vids"),result["vids"],"migdep-vid-cont","migdep-slide-vid",(ancho >= 792 ? 3 : (ancho >= 545 ? 2 : 1)),(ancho >= 792 ? 0.3 : (ancho >= 545 ? 0.4 : 0.47)),true,"migdep");
+				});
+
+				$(document).ready(function($) {
+					$('a[data-rel^=lightcase]').lightcase();
+				});
+
+				}
+			});
+
+			$(window).on("resize",function(){
+						var ancho = Math.max(document.documentElement.clientWidth, window.innerWidth || document.body.ClientWidth || 0);
+						renderSlider("trades",$("#trades-container"),$("#trades-cont"),$("#trades-vids"),result["vids"],"trades-vid-cont","trades-slide-vid",(ancho >= 792 ? 3 : (ancho >= 545 ? 2 : 1)),(ancho >= 792 ? 0.3 : (ancho >= 545 ? 0.4 : 0.47)),true,"trades");
+			});
+
+			$(document).ready(function($) {
+				$('a[data-rel^=lightcase]').lightcase();
+			});
+
+			}
+		});
+
+	}
+});
