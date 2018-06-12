357515fe9ba32c9788f4e75d9781df6e9b2a2032
colmexdev <github@colmex.mx>
2018-06-12 12:17:07 -0500

    Orden de videos

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -121,6 +121,7 @@ function listo(){
 			success: function(result){
 				var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
 				$("#vid-historias").html(html);
+			$('a[data-rel^=lightcase]').lightcase();
 			}
 		}).done(function(){
 			$.ajax({
@@ -130,22 +131,23 @@ function listo(){
 				success: function(result){
 					var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
 					$("#vid-talk").html(html);
+					$('a[data-rel^=lightcase]').lightcase();
 				}
 			});
 		}).done(function(){
-					$.ajax({
-						url: "http://pred1.colmex.mx/catalogo_videos.json?titulo=Lanzamiento%20del%20informe%20Desigualdades%20en%20M%C3%A9xico%202018",
-						method: "get",
-						dataType: "JSON",
-						success: function(result){
-							var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
-							$("#vid-stream").html(html);
-						}
-					});
+			$.ajax({
+				url: "http://pred1.colmex.mx/catalogo_videos.json?titulo=Lanzamiento%20del%20informe%20Desigualdades%20en%20M%C3%A9xico%202018",
+				method: "get",
+				dataType: "JSON",
+				success: function(result){
+					var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive"></a>'
+					$("#vid-stream").html(html);
+					$('a[data-rel^=lightcase]').lightcase();
+				}
+			});
 		}).done(function(){
 			isotopeMedios();
 			$("head").append('<script id="script-twitter" async type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>');
-			$('a[data-rel^=lightcase]').lightcase();
 		});
 
 	} else if(window.location.pathname == "/videos"){
