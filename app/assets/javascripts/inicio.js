226e3cc177d8787db8a873886fd7f49240ab78db
colmexdev <github@colmex.mx>
2018-11-23 12:14:31 -0600

    Informe inglés

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -113,16 +113,25 @@ function listo(){
 	if(window.location.pathname == "/"){
 		$("head").append('<script id="script-twitter" async type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>');		
 	} else if(window.location.pathname == "/informe2018"){
-
 		$.ajax({
-			url: "https://coed.colmex.mx/catalogo_videos.json?titulo=Informe%20Desigualdades%20en%20M%C3%A9xico%202018.%20Dos%20historias%20de%20vida",
-			method: "get",
-			dataType: "JSON",
+			url: "https://coed.colmex.mx/catalogo_videos.json?titulo=Desigualdades en México 2018: una mirada interdisciplinaria",
 			success: function(result){
-				var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive vid-clip"></a>'
-				$("#vid-historias").html(html);
-				$('a[data-rel^=lightcase]').lightcase();
+					var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive vid-clip"></a>'
+					$("#vid-inf").html(html);
+					$('a[data-rel^=lightcase]').lightcase();
+				}
 			}
+		}).done(function(){
+			$.ajax({
+				url: "https://coed.colmex.mx/catalogo_videos.json?titulo=Informe%20Desigualdades%20en%20M%C3%A9xico%202018.%20Dos%20historias%20de%20vida",
+				method: "get",
+				dataType: "JSON",
+				success: function(result){
+					var html = '<a href="https://www.youtube-nocookie.com/embed/' + result["vids"][0]["v_id"] + '?rel=0&amp;autoplay=1" data-rel="lightcase:informeVideos"><img src="' + result["vids"][0]["thumbnail"].replace("default.jpg","hqdefault.jpg") + '" alt="' + result["vids"][0]["titulo"] + '" class="img-responsive vid-clip"></a>'
+					$("#vid-historias").html(html);
+					$('a[data-rel^=lightcase]').lightcase();
+				}
+			});
 		}).done(function(){
 			$.ajax({
 				url: "https://coed.colmex.mx/catalogo_videos.json?titulo=Desigualdades%20en%20M%C3%A9xico%202018.%20Entrecruzamiento%20y%20acumulaci%C3%B3n%20de%20desventajas",
